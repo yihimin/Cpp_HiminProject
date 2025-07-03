fcd9a57464b06d6db5f94cc7f1a633cbf5695a7a
yihimin <yihimin01@gmail.com>
2025-07-03 11:54:43 +0900

    main.cpp 파일 수정

---
+++ HiminProject/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    string numbers;
+    cin >> N;
+    cin >> numbers;
+
+    int sum = 0;
+    for (int i = 0; i < numbers.length(); i++)
+    {
+        sum += numbers[i]-'0';
+    }
+    cout << sum << "\n";
+}
