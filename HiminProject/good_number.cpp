ee60cb98434c42fe2636d66a0eb21965a85e11fe
yihimin <yihimin01@gmail.com>
2025-07-04 13:59:36 +0900

    good_number 파일 추가 및 함수 구현
    HiminProject.vcxproj에 `good_number.cpp`와 `good_number.h` 파일을 추가했습니다.
    이 파일들은 '소스 파일\problem2' 필터 아래에 위치하며,
    `good_number.cpp`에는 주어진 수의 배열에서 특정 조건을 만족하는 쌍의 개수를 세는 `solve_good_number` 함수가 구현되었습니다.
    `good_number.h`에는 해당 함수의 선언이 포함되어 있습니다.

---
+++ HiminProject/good_number.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void solve_good_number()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+    vector<int> A(N, 0);
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+    sort(A.begin(), A.end());
+    int count = 0;
+    for (int k = 1; k < N; k++) {
+        long find = A[k];
+        int i = 0;
+        int j = k - 1;
+        // 작성하세요.  
+         while (i < j) {
+        long sum = A[i] + A[j];
+        if (sum == find) {
+            count++;
+			break; // 쌍을 찾았으면 더 이상 탐색할 필요 없음
+        }
+        else if (sum < find) {
+            i++; // 합이 작으면 왼쪽 수 키워야 하므로 i++
+        }
+        else {
+            j--; // 합이 크면 오른쪽 수 줄여야 하므로 j--
+        }
+    }
+    cout << count << "\n";
+}
